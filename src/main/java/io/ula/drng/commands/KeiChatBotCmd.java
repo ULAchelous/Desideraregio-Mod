05112cb2e54f01b0e8fbba8b60886adf25bedc48
zyhfunny <zyhfunny@163.com>
2026-08-28 19:12:22 +0800

    - refactored ai skill api

--- src/main/java/io/ula/drng/commands/KeiChatBotCmd.java
+++ src/main/java/io/ula/drng/commands/KeiChatBotCmd.java
@@ -4,6 +4,10 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.ula.drng.utils.kei.KeiChatBotUtils;
+import io.ula.drng.utils.kei.tool.FuncTool;
+import io.ula.drng.utils.kei.tool.Tool;
+import io.ula.drng.utils.kei.tool.Tools;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
@@ -16,6 +20,20 @@ public class KeiChatBotCmd {
                     .executes(commandContext -> {
                         KeiChatBotUtils.clearContext();
                         commandContext.getSource().getPlayer().sendSystemMessage(Component.literal("上下文已清除"));
+                        return 0;
+                    })
+                    .requires(commandSourceStack -> commandSourceStack.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER))
+            )
+            .then(Commands.literal("tools")
+                    .executes(commandContext -> {
+                        commandContext.getSource().getPlayer().sendSystemMessage(Component.literal("已加载的工具:"));
+                        for(Tool tool : Tools.TOOLS){
+                            Component perfix = Component.empty();
+                            if(perfix instanceof FuncTool funcTool) {
+                                perfix = Component.literal("function").withStyle(ChatFormatting.YELLOW);
+                                commandContext.getSource().getPlayer().sendSystemMessage(perfix.copy().append(Component.literal(funcTool.getName()).withStyle(ChatFormatting.AQUA)));
+                            }
+                        }
                         return 0;
                     })
                     .requires(commandSourceStack -> commandSourceStack.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER))
